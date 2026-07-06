d73ee912581863d7adf83986e1a08622a29eabf6
goring <dlrkdms001@gmail.com>
2026-07-06 15:15:05 +0900

    fix: 수상일 수정

--- src/constants/education/awards.ts
+++ src/constants/education/awards.ts
@@ -5,13 +5,13 @@ export const awards: AwardItem[] = [
     name: "SSAFY X Kakao tech bootcamp AI Hackathon 카카오 대표상",
     competition: "",
     organization: "",
-    date: "2026.06",
+    date: "2026.06.14",
   },
   { 
     name: "삼성 청년 SW·AI 아카데미 2학기 프로젝트 우수상 (1등)",
     competition: "",
     organization: "",
-    date: "2026.05",
+    date: "2026.05.21",
   },
   {
     name: "한남대학교 캡스톤 디자인 대회 우수상",
